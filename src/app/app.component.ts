4080230da2eba756ddab7762cae2380773f130c2
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-03 01:45:33 +0300

    made the first http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FormsModule,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'forms';
+
+  constructor(private http:HttpClient) { }
+
+  submitForm(form: NgForm): void {
+    this.http.post('https://blog-7f459-default-rtdb.firebaseio.com/posts.json',form.value).subscribe(res=>{
+      console.log(res);
+    })
+    console.log(form.value);
+  }
 }
