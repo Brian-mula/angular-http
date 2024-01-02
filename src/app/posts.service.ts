c1ba45f3c8c44bea168503877d740bfb67267d4d
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-03 02:29:59 +0300

    added posts module

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,14 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { map } from "rxjs";
 import { PostType } from "./types/post-type";
 
 @Injectable({providedIn:'root'})
 export class PostsService{
 constructor(private http:HttpClient){}
 
+posts:PostType[]=[];
+
 async createPost(post:PostType){
     this.http.post('https://blog-7f459-default-rtdb.firebaseio.com/posts.json',post).subscribe(res=>{
       console.log(res);
+    })
+}
+async getposts(){
+    this.http.get<PostType[]>('https://blog-7f459-default-rtdb.firebaseio.com/posts.json').pipe(map(resData=>{
+        const postArray=[];
+        for (const key in resData){
+            if(resData.hasOwnProperty(key)){
+                postArray.push({...resData[key],id:key})
+            }
+            
+        }
+        return postArray;
+    })).subscribe(res=>{
+        this.posts=res;
     })
 }
 }
