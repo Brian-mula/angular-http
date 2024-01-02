e00e94b4d9b3ea525f1182e2dcf05bd8a329959d
Brian-mula <mulatiwangilabrian@gmail.com>
2024-01-03 02:29:55 +0300

    added posts module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { PostsService } from './posts.service';
+import { PostsComponent } from "./posts/posts/posts.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet,FormsModule,HttpClientModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, FormsModule, HttpClientModule, PostsComponent]
 })
-export class AppComponent {
+export class AppComponent{
   title = 'forms';
+  
 
-  constructor(private http:HttpClient,
-    private postService:PostsService
+  constructor(
+    
     ) { }
 
-  submitForm(form: NgForm): void {
-    this.postService.createPost(form.value);
-    console.log(form.value);
-  }
+  
 }
